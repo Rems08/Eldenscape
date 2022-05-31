685761d056193d13b9d05b49a859e3cd52d17bc2
Rems08 <remymassiet@gmail.com>
2022-05-31 11:40:46 +0200

    Create login

--- connexion.php
+++ connexion.php
@@ -9,29 +9,40 @@
 <body>
   <div id="main">
     <div><a href="index.html"><img src="images/logo/logo.png" alt="logo de Eldenscape" id="logo" style="text-align: center;"></a></div>
-    <div id="formulaire">
+    <form action="verification.php" method="POST">
+      <div id="formulaire">
         <div class="form">
             <div class="title">CONNEXION</div>
             <div class="subtitle">Connectez-vous</div>
             <div class="input-container ic2">
-              <input id="email" class="input" type="text" placeholder=" " />
+              <input id="email" class="input" type="text" placeholder=" " name="username" required/>
               <div class="cut cut-short"></div>
               <label for="email" class="placeholder">Email</label>
             </div>
             <div class="input-container ic1">
-              <input id="password" class="input" type="password" placeholder=" " />
+              <input id="password" class="input" type="password" placeholder=" " name="password" required/>
               <div class="cut"></div>
               <label for="password" class="placeholder">Password</label>
             </div>
 
-            <button type="text" class="submit">Connexion</button>
+            <button type="text" class="submit" value='LOGIN'>Connexion</button>
             <p>
               &lowbar;
             </p>
             <p>Vous n'avez pas encore de compte ?</p>
             <a href="inscription.html"><button type="text" class="inscription">Inscription</button></a>
+            <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Veuillez réessayer</p>";
+                        echo "<script type='text/javascript'>alert('Utilisateur ou mot de passe incorrect');</script>";
+                }
+                ?>
           </div>
-        </div>
-      </div>
+          </form>
+    </div>
+  </div>
+      
 </body>
 </html>
