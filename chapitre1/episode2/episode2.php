7adf30fba3a569c9af727eb25d3ebfa8ebfb08f4
Rems08 <remymassiet@gmail.com>
2022-06-07 14:08:26 +0200

    Add attached files

--- chapitre1/episode2/episode2.php
+++ chapitre1/episode2/episode2.php
@@ -12,7 +12,7 @@
         <h1>Lancer la mystérieuse machine</h1>
         <h2>Vous venez d'entrer dans la salle super sécurité !</h2>
         <p>Une fois la porte hackée, vous entrez maintenant dans la salle et découvrez la mystérieuse machine. Vous devez maintenant
-            trouver comment lancer cette dernière... Sur le bureau vous trouvez un mystérieux dossier composé de 2 documents <a href="/chapitre1/episode2/documents/code.jpg" download>Une note</a> et <a href="/chapitre1/episode2/documents/ASCII.jpg" download>Un étrange tableau</a>
+            trouver comment lancer cette dernière... Sur le bureau vous trouvez un mystérieux dossier composé de 2 documents <a href="/chapitre1/episode2/documents/code.jpg" download>Une note</a> et <a href="/chapitre1/episode2/documents/ASCII.jpg" download>Un étrange tableau</a>...
         </p>
         <form action="ep2Validation.php" method="POST">
             <div class="box-input-validation">
