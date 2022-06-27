f831551f9660f185d245a95cbfaca46bf307907e
Léo Richez <75373617+HerosFunk@users.noreply.github.com>
2022-06-27 08:40:18 +0200

    correction lien href

--- chapitre1/episode2/episode2.php
+++ chapitre1/episode2/episode2.php
@@ -3,16 +3,16 @@
 <head>
   <meta charset="utf-8">
   <title>Accueil Eldenscape</title>
-  <link rel="stylesheet" href="/style.css">
+  <link rel="stylesheet" href="../../style.css">
   <script src="script.js"></script>
 </head>
 <body>
-    <a href="/index.html"><img src="/images/logo/logo.png" alt="logo de Eldenscape" id="logo"></a>
+    <a href="./index.html"><img src="../../images/logo/logo.png" alt="logo de Eldenscape" id="logo"></a>
     <div id="welcome">
         <h1>Lancer la mystérieuse machine</h1>
         <h2>Vous venez d'entrer dans la salle super sécurité !</h2>
         <p>Une fois la porte hackée, vous entrez maintenant dans la salle et découvrez la mystérieuse machine. Vous devez maintenant
-            trouver comment lancer cette dernière... Sur le bureau vous trouvez un mystérieux dossier composé de 2 documents <a href="/chapitre1/episode2/documents/code.jpg" download>Une note</a> et <a href="/chapitre1/episode2/documents/ASCII.jpg" download>Un étrange tableau</a>...
+            trouver comment lancer cette dernière... Sur le bureau vous trouvez un mystérieux dossier composé de 2 documents <a href="./documents/code.jpg" download>Une note</a> et <a href="./documents/ASCII.jpg" download>Un étrange tableau</a>...
         </p>
         <form action="ep2Validation.php" method="POST">
             <div class="box-input-validation">
