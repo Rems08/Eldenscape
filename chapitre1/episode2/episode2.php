abcba93f793e3473ffbb2790eddc620423849681
Florian Cop <floriancopitet@gmail.com>
2022-06-28 15:46:53 +0200

    Back-Finitot

---
+++ chapitre1/episode2/episode2.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Accueil Eldenscape</title>
+  <link rel="stylesheet" href="/style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+    <a href="/index.html"><img src="/images/logo/logo.png" alt="logo de Eldenscape" id="logo"></a>
+    <div id="welcome">
+        <h1>Lancer la mystérieuse machine</h1>
+        <h2>Vous venez d'entrer dans la salle super sécurité !</h2>
+        <p>Une fois la porte hackée, vous entrez maintenant dans la salle et découvrez la mystérieuse machine. Vous devez maintenant
+            trouver comment lancer cette dernière... Sur le bureau vous trouvez un mystérieux dossier composé de 2 documents <a href="/chapitre1/episode2/documents/code.jpg" download>Une note</a> et <a href="/chapitre1/episode2/documents/ASCII.jpg" download>Un étrange tableau</a>...
+        </p>
+        <form action="ep2Validation.php" method="POST">
+            <div class="box-input-validation">
+                <div class="input-container-validation">
+                    <input id="password" class="input-validation" type="text" placeholder=" " name="password-ep2" required/>
+                    <div class="cut-validation"></div>
+                    <label for="password" class="placeholder">Password</label>
+                </div>
+                
+                <a href="#"><button class="custom-btn btn-main">Valider</button></a>
+                <?php
+                    if( $_SESSION['err'] == true){
+                        $err_pass = "Le password n'est pas bon";
+                        echo $err_pass;
+                    }
+                ?>
+               
+                </div>
+            </div>
+        </form>
+    
+    </div>
+</body>
+</html>
--- principale.php
+++ principale.php
@@ -1,21 +1,60 @@
+<?php
+//        <!-- tester si l'utilisateur est connecté -->
+    session_start();
+    include_once('connexionBD.php');     
+    if(isset($_SESSION['id'])){
+        $user = $_SESSION['username'];
+        // afficher un message
+        $bonjour = "Bonjour $user, vous êtes connecté";
+    }else{
+        header("location:connexion.php");
+    }  
+?>
 <html>
     <head>
         <meta charset="utf-8">
         <!-- importer le fichier de style -->
         <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
     </head>
-    <body style='background:#fff;'>
-        <div id="content">
-            <!-- tester si l'utilisateur est connecté -->
-            <?php
-                session_start();
-                if($_SESSION['username'] !== ""){
-                    $user = $_SESSION['username'];
-                    // afficher un message
-                    echo "Bonjour $user, vous êtes connecté";
+    <body>
+    <a href="/index.html"><img src="images/logo/logo.png" alt="logo de Eldenscape" id="logo"></a>
+    <div id="welcome">
+
+        <h2>Vous êtes prêt à continuer votre aventure ?</h2>
+        <a href="verifChallenge.php"><button class="custom-btn btn-main">continuer</button></a>
+        <?php
+        if(isset($_GET['nochallenge']))
+                { 
+                   if($_GET['nochallenge']==true)
+                   {  
+                      $nochallenge = "Vous n'avez pas encore commencer l'aventure, cliquer sur Nouvelle Partie";
+                   }
+                }
+        ?>
+        <?php
+            if(isset($nochallenge)){
+                echo $nochallenge;
+            }
+        ?>
+        <a href='chapitre1/episode1/MineSweeper.php'><button class="custom-btn btn-second">Nouvelle Partie</button></a>
+        <a href='deconnexion.php'><button class="custom-btn btn-warning">Deconnexion</button></a>
+        <a href='principale.php?deconnexion=true'><button class="custom-btn btn-warning">Deconnexion</button></a>
+        <!-- tester si l'utilisateur est connecté -->
+        <?php
+                if(isset($bonjour)){
+                  echo $bonjour;
+                }
+                ?>
+        <?php
+                if(isset($_GET['deconnexion']))
+                { 
+                   if($_GET['deconnexion']==true)
+                   {  
+                      session_unset();
+                      header("location:connexion.php");
+                   }
                 }
             ?>
-            
-        </div>
+    </div>
     </body>
 </html>
